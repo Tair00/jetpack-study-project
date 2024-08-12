a7841fc27ead625faa9d8715bbdc1f9a94004261
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-12 19:38:52 +0300

    updateProgress

--- app/src/main/java/com/example/compositionapp/presentation/GameViewModel.kt
+++ app/src/main/java/com/example/compositionapp/presentation/GameViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.compositionapp.presentation
 
+import android.app.Application
 import android.os.CountDownTimer
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.compositionapp.R
 import com.example.compositionapp.data.GameRepositoryImpl
 import com.example.compositionapp.domain.entity.GameSettings
 import com.example.compositionapp.domain.entity.Level
@@ -12,24 +15,29 @@ import com.example.compositionapp.domain.repository.GameRepository
 import com.example.compositionapp.domain.usecases.GenerateQuestionUseCase
 import com.example.compositionapp.domain.usecases.GetGameSettingsUseCase
 
-class GameViewModel: ViewModel() {
+class GameViewModel(application: Application): AndroidViewModel(application) {
     private val repository = GameRepositoryImpl
     private lateinit var gameSettings: GameSettings
     private lateinit var level: Level
+    private val context = application
     private val  generatedQuestionUseCase = GenerateQuestionUseCase(repository)
     private val  getGameSettingUseCase = GetGameSettingsUseCase(repository)
     private val _formattedTime = MutableLiveData <String>()
+        val formattedTime : LiveData<String>
+        get() = _formattedTime
     private var timer: CountDownTimer? = null
-
+    private val _progressAnswers = MutableLiveData <String>()
+    val progressAnswers : LiveData<String>
+        get() = _progressAnswers
     private val  _question = MutableLiveData<Question>()
     val question: LiveData<Question>
         get() = _question
     private var countOfRightAnswer = 0
     private var countOfQuestions = 0
 
-
-    val formattedTime: LiveData<String>
-        get() = _formattedTime
+    private val _percentOfRightAnswers = MutableLiveData<Int>()
+    val percentOfRightAnswers: LiveData<Int>
+        get() = _percentOfRightAnswers
 
     fun startGame(level: Level){
         getGameSettings(level)
@@ -56,7 +64,18 @@ class GameViewModel: ViewModel() {
         _question.value = generatedQuestionUseCase(gameSettings.maxSumValue)
 
     }
-
+    private fun updateProgress(){
+        val percent = calculatePercentOfRightAnswers()
+        _percentOfRightAnswers.value = percent
+        _progressAnswers.value =String.format(
+            context.resources.getString(R.string.progress_answers),
+            countOfRightAnswer,
+            gameSettings.minCountOfRightAnswers
+        )
+    }
+    private fun calculatePercentOfRightAnswers(): Int{
+        return ((countOfRightAnswer/countOfQuestions.toDouble())*100).toInt()
+    }
     fun startTimer() {
          timer = object : CountDownTimer(
              gameSettings.gameTimeInSeconds * MILLIS_IN_SECONDS,
