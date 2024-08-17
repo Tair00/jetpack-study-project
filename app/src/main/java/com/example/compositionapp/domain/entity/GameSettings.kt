5514e0762ade82cdd56ed03100804f7ed29d37e7
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-17 20:01:04 +0300

    Binding

--- app/src/main/java/com/example/compositionapp/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/compositionapp/domain/entity/GameSettings.kt
@@ -1,10 +1,18 @@
 package com.example.compositionapp.domain.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.io.Serializable
-
+@Parcelize
 class GameSettings (
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
     val minPercentOfRightAnswers: Int,
     val gameTimeInSeconds: Int
-):Serializable
+):Parcelable{
+    val minCountOfRightAnswersString: String
+        get() = minCountOfRightAnswers.toString()
+    val minPercentOfRightAnswersString: String
+        get() = minPercentOfRightAnswers.toString()
+
+}
