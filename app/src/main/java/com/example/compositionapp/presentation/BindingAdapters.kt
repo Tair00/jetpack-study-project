6bf9ef0aa800dfdecadad68d9f4ccd5925087b9a
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-18 09:45:21 +0300

    Binding

--- app/src/main/java/com/example/compositionapp/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/compositionapp/presentation/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.example.compositionapp.presentation
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.compositionapp.R
+import com.example.compositionapp.domain.entity.GameResult
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView,count : Int){
@@ -12,9 +13,30 @@ fun bindRequiredAnswers(textView: TextView,count : Int){
             )
 }
 @BindingAdapter("scoreAnswers")
-fun bindScoreAnswers(textView: TextView,count : Int){
+fun bindScoreAnswers(textView: TextView, score : Int){
     textView.text = String.format(
         textView.context.getString(R.string.score_answers),
-        count
+        score
     )
+}
+@BindingAdapter("requirePercentage")
+fun bindRequirePercentage(textView: TextView, percentage : Int){
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answers),
+        percentage
+    )
+}
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult){
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answers),
+        getPercentOfRightAnswers(gameResult)
+    )
+}
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        0
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
 }
